c5ecb343268f8418d254fbe4726a3abd51d4fb68
vireshjivane <viresh.jivane@sjsu.edu>
2015-05-05 16:47:20 -0700

    Commit - AOP

--- src/main/java/com/quiz/aop/QuizLogging.java
+++ src/main/java/com/quiz/aop/QuizLogging.java
@@ -3,10 +3,14 @@ package com.quiz.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
 
 @Aspect
 public class QuizLogging {
 
+	
+	/*
 	@Around("execution(* *.*(..))")
 	public void quizAppBasicLogging(ProceedingJoinPoint proceedingJoinPoint){
 		try{
@@ -16,6 +20,6 @@ public class QuizLogging {
 		}catch(Throwable e){
 			e.printStackTrace();
 		}
-	}
+	}*/
 	
 }

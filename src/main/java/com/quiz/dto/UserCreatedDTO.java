6528297122e191b4e292af747a54af7208317139
Puneet Popli <puneetpopli.26pp@gmail.com>
2015-05-05 20:40:51 -0700

    User created DTO added

---
+++ src/main/java/com/quiz/dto/UserCreatedDTO.java
@@ -0,0 +1,46 @@
+package com.quiz.dto;
+
+public class UserCreatedDTO 
+{
+
+	
+	String quizName;
+	String quizCreator;
+	String category;
+	Integer maxScore;
+	String topper;
+	
+	
+	public String getQuizName() {
+		return quizName;
+	}
+	
+	public void setQuizName(String quizName) {
+		this.quizName = quizName;
+	}
+	public String getQuizCreator() {
+		return quizCreator;
+	}
+	public void setQuizCreator(String quizCreator) {
+		this.quizCreator = quizCreator;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Integer getMaxScore() {
+		return maxScore;
+	}
+	public void setMaxScore(Integer maxScore) {
+		this.maxScore = maxScore;
+	}
+	public String getTopper() {
+		return topper;
+	}
+	public void setTopper(String topper) {
+		this.topper = topper;
+	}
+	
+}
